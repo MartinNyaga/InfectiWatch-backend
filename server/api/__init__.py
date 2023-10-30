b7878b91611966eb12c66053b6d8c7f2874db697
Martin Nyaga <martin.nyaga@student.moringaschool.com>
2023-10-30 19:49:21 +0300

    created models

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -1,1 +1,22 @@
-# schema
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+from api import routes
--- server/app.py
+++ server/app.py
@@ -1,1 +1,5 @@
-# schema
+from api import app
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
